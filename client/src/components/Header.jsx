f2c9009c2ee70b01a66194efb46d8bba67f549fa
ayushbirla71 <114582571+ayushbirla71@users.noreply.github.com>
2023-04-28 12:36:22 +0530

    complete

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Containter from 'react-bootstrap/Container';
-
 const Header = () => {
   return (
     <div style={{ backgroundColor :"black", color:"white"}}>
-    <h1 style={{margin:"0px",paddingLeft:"2%",height:"50px"}}>Notes</h1>
+    <h1 style={{margin:"0px",paddingLeft:"2%",height:"50px"}}>Mobile Number Details</h1>
     </div>
   )
 }
--- client/src/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const fetchAPI = ()=>{
-    try {
-        let data = axios('url')
-    } catch (error) {
-        
-    }
-}
